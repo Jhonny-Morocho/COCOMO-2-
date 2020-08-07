75f75b99e8a56bbc639d49b30aa7fcbcaee80683
Jose Andres Cartuche (Aceleración) <joseandres.cv@hotmail.com>
2020-08-07 01:05:11 -0500

    Lectura de lineas de codigo fuente de los archivos de un directorio e inclusión de archivos excluidos generales.

--- controler/appCocomos.js
+++ controler/appCocomos.js
@@ -8,9 +8,12 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 }
 
 var extensions = "*.js,*.php,*.html,*.css,*.json,*.java"; 			// The default extensions that are checked
-var typeCommentInline = "^[\s\t]*\u002f\u002f.*?$,^[\s\t]*'.*?$";   //Comments identified by default of a single line: // y '.
-var typeBlockCommentOpen = new Array(new RegExp(".*/\*.*"));
-var typeBlockCommentClose = new Array(new RegExp(".*[\*/]"));
+var typeCommentInline = "^[\s\t]*\u002f\u002f.*?$";   				//Comments identified by default of a single line: //.
+var typeBlockCommentOpen = ".*(\u002f\\*).*,.*(<!--).*";			// Block comment opening character /* <!--
+var typeBlockCommentClose = ".*(\\*\u002f),.*(-->)";				// Block comment closing character */ -->
+var exclusions = ".git,.gitignore,.gitattributes,.svn,bin,obj,properties";
+
+var regCommentInLine, regBlockCommentOpen, regBlockCommentClose;
 
 var n_folders, n_pfiles, n_lines, n_lWhite, n_comments;
 
@@ -49,6 +52,27 @@ function processDirectory(files)
 	var exts = extensions.split(",").map((item) => item.replace("*", ""));
 	console.log(exts);
 
+	var expComentary = typeCommentInline.split(",");
+	regCommentInLine = new Array();
+	expComentary.forEach((exp) => regCommentInLine.push(new RegExp(exp, "ims")));
+	console.log(expComentary);
+	console.log(regCommentInLine);
+
+	var expBlockComentaryOpen = typeBlockCommentOpen.split(",");
+	regBlockCommentOpen = new Array();
+	expBlockComentaryOpen.forEach((exp) => regBlockCommentOpen.push(new RegExp(exp)));
+	console.log(expBlockComentaryOpen);
+	console.log(regBlockCommentOpen);
+
+	var expBlockComentaryClose = typeBlockCommentClose.split(",");
+	regBlockCommentClose = new Array();
+	expBlockComentaryClose.forEach((exp) => regBlockCommentClose.push(new RegExp(exp)));
+	console.log(expBlockComentaryClose);
+	console.log(regBlockCommentClose);
+
+	var excls = exclusions.split(",");
+	console.log(excls);
+
 	for (f of files) 
 	{
 		var b = false;
@@ -58,6 +82,15 @@ function processDirectory(files)
 			{
 				b = true;
 				accountLinesCode(f);
+				break;
+			}
+		}
+		for (x of excls) {
+			var re = new RegExp("^" + x + "$");
+			if (f.name.search(re) != -1)
+			{
+				b = true;
+				console.log(`%c${f.webkitRelativePath} (excluida)`, "color:#D358F7");
 				break;
 			}
 		}
@@ -97,30 +130,78 @@ function accountLinesCode(file)
 		//console.log(e.target.result);
 		var isBlank = false;
 		var isCommentary = false;
+		var numComment = 0, numBlank = 0, numLines = 0;
 
 		// Separo las lineas de codigo por líneas
 		var lines = e.target.result.split(/\r\n|\n/);
 		lines.forEach(function(line) {
 			// Comprueba si estoy en un bloque de comentario
-			if (blockComentary) {
-				isComment = true;
+			if (blockComentary) 
+			{
+				isCommentary = true;
 
 				//Ver si la línea actual cierra el comentario o no
-				if (line.match(typeBlockCommentClose[iBlockComment]))
+				if (line.search(regBlockCommentClose[iBlockComment]) != -1)
 				{
 					// Ya no se esta en un bloque de comentario
 					blockComentary = false;
 					iBlockComment = -1;
 				}
 			}
-			else { // Linea de código normal
+			else // Linea de código normal
+			{
+				// Ver si es una linea vacía
+				isBlank = isNullOrWhiteSpace(line);
 
 				// Comprobar si es un comentario de una sola línea
 				isCommentary = false;
+				regCommentInLine.forEach((re) => {
+					if (line.search(re) != -1) {
+						isCommentary = true;
+					}
+				});
+
+				// Verificar si es un comentario de varias lineas (Bloque de comentario)
+				// Ver si la linea actual es una apertura de comentario
+				for (var i = 0; i < regBlockCommentOpen.length; i++) {
+					// Si es un bloque de comentario
+					if (line.search(regBlockCommentOpen[i]) != -1) { 
+						// Si no se cierra el comentario en la misma línea
+						if (line.search(regBlockCommentClose[i]) == -1) 
+						{
+							// Es un bloque de comentario de varias lineas
+							// Se marca a partir de ahora que las siguientes lineas son comentarios
+							// hasta que aparesca el cierre del comentario
+							iBlockComment = i;
+							blockComentary = true;
+						}
+						isCommentary = true;
+						break;
+					}
+				}
+			}
+			if (isCommentary) {
+				numComment++;
+			}
+			else if (isBlank) {
+				numBlank++;
+			}
+			else {
+				numLines++;
 			}
 		});
+		console.log(`%c${file.name}:\n
+			Líneas de Código ejecutables: ${numLines}\n
+			Líneas en blanco: ${numBlank}\n
+			Lineas comentadas: ${numComment}\n
+			Líneas totales: ${lines.length}`, "color:#FF4000");
 	};
 	reader.readAsText(file);
 
 	n_pfiles++;
+}
+
+function isNullOrWhiteSpace(input) 
+{
+  return !input || (typeof input === "string" && !input.trim());
 }
