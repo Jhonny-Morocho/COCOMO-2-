6b5b0870b2f41e81d6c0f1fc133a1f437b3ec3bb
Jose Andres Cartuche (Aceleración) <joseandres.cv@hotmail.com>
2020-08-22 03:17:45 -0500

    Se inicializa el div para subir archivos con el metodo drag & drop, configurando sus mensajes de salida

--- appMTD/js/appIndex.js
+++ appMTD/js/appIndex.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
 
     // Material Select Initialization
     $('.mdb-select').materialSelect();
+
+    var messages = {
+		default: "Arrastre y suelte una carpeta o haga clic aquí",
+		replace: "Arrastra y suelte o haz clic para reemplazar",
+		remove: "Eliminar",
+		error: "Vaya, algo sucedió mal."
+	};
+	$('#dir_files').data("messages", messages);
+
+    // Init File Upload
+	$('.file-upload').file_upload();
 });
